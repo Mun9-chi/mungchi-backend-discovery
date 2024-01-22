698693e455cc13175e5d0784d38739c7754c17b8
ingpyo <juninpyo726@naver.com>
2024-01-22 17:06:44 +0900

    [BE] feat: 프로젝트 초기설정

---
+++ backend/src/main/java/org/mungchi/backend/MungchiApplication.java
@@ -0,0 +1,13 @@
+package org.mungchi.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MungchiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/mungchi/backend/MungchiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mungchi.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MungchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
